d72f866a7a26fba10ed67147e17584898786b8db
Brinkslu991 <153300807+Brinkslu991@users.noreply.github.com>
2024-09-23 11:17:03 -0400

    tweryjkgulh,.

---
+++ main.py
@@ -0,0 +1,13 @@
+# Lucas Brinks
+# 9/23/24
+# psuedocode
+
+print('Area of  a Triangle')
+
+base = float(input('Enter Base #: '))
+height = float(input('Enter Height #: '))
+
+area = (base * height) / 2
+
+print('This is the area of your Triangle')
+print(area)
