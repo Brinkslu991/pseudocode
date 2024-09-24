71f38d35dea2be2d21bda6ec3be4635b8a513e7b
Brinkslu991 <153300807+Brinkslu991@users.noreply.github.com>
2024-09-24 11:04:59 -0400

    gfhj

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 # Lucas Brinks
-# 9/23/24
+# 9/24/24
 # psuedocode
 
-print('Area of  a Triangle')
+#Average quiz score
+name = input('Enter your name: ')
+print(f'Welcome {name}')
 
-base = float(input('Enter Base #: '))
-height = float(input('Enter Height #: '))
+quiz1 = int(input('Enter your first quiz score: '))
+quiz2 = int(input('Enter your second quiz score: '))
+quiz3 = int(input('Enter your third quiz score: '))
 
-area = (base * height) / 2
+sum = quiz1 + quiz2 + quiz3
 
-print('This is the area of your Triangle')
-print(area)
+average = (sum/3)
+
+print('Your quiz scores and average:')
+print(quiz1)
+print(quiz2)
+print(quiz3)
+print(average)
+
+fname = input('Enter your name: ')
+print(f'welcome {fname}')
+miles = float(input('How many miles did you drive today: '))
+gallons = float(input('How many gallon of gas did your car use: '))
+
+mpg = (miles / gallons)
+
+print(f'You drove {miles} miles')
+print(f'You used {gallons} gallons')
+print(f'You get {mpg} miles per gallon')
